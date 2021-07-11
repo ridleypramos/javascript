bafda6d29a39b2b26c5032f9a1a80473ecbb2281
ridleypramos <ridleypr@gmail.com>
2021-07-10 21:33:34 -0300

    Delete #11 directory

--- #11/ex008.js
+++
@@ -1,8 +0,0 @@
-var velocidade = 59
-console.log('A velocidade do eu carro é ' + velocidade + 'km/h')
-
-if (velocidade >= 60){
-    console.log('Você ultrapassou o limite de velocidade')
-} else{
-console.log('Dirija sempre usando  o cinto de segurança!')
-}
