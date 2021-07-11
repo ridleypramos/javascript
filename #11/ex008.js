94f6d8b37b7e3aea32013dfd62da22c294fcd0b9
ridleypramos <ridleypr@gmail.com>
2021-07-10 21:29:54 -0300

    Add files via upload

---
+++ #11/ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 59
+console.log('A velocidade do eu carro é ' + velocidade + 'km/h')
+
+if (velocidade >= 60){
+    console.log('Você ultrapassou o limite de velocidade')
+} else{
+console.log('Dirija sempre usando  o cinto de segurança!')
+}
